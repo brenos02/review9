5651970125c9481df531ed90fdc02627d1a0b342
Breno Sallouti <brenosallouti@icloud.com>
2017-06-12 10:13:24 -0300

    Finished review 9

--- Review9/main.cpp
+++ Review9/main.cpp
@@ -4,12 +4,22 @@
 //
 //  Created by Breno Sallouti on 09/06/17.
 //  Copyright © 2017 Breno Sallouti. All rights reserved.
-//
+
+// Get input from user of 10 numbers and calculate its average
 
 #include <iostream>
 
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int numbers[10];
+    int i;
+
+    for(int i = 0; i < 10 ; i++){
+        std::cout << "Give me a value:";
+        std::cin >> numbers[i];
+    }
+    i= numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]+numbers[8]+numbers[9];
+    
+    std::cout << "The average of those values is:"<< i /10;
     return 0;
 }
